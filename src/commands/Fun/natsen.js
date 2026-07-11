d706a95a4d84e593327f3c79577a4ce74d196b4a
swaggydoggs <shaloma885@gmail.com>
2026-07-11 16:07:43 -0500

    Create natsen.js

---
+++ src/commands/Fun/natsen.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('natsen')
+        .setDescription('Shows that Natsen is tuff'),
+
+    async execute(interaction) {
+        await interaction.reply('Natsen is tuff 🔥');
+    },
+};

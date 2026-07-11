096e770cd0a19043b6795484226d35348c190d43
swaggydoggs <shaloma885@gmail.com>
2026-07-11 08:12:37 -0500

    Update owner.js

--- src/commands/Core/owner.js
+++ src/commands/Core/owner.js
@@ -1,13 +1,4 @@
-src
- └── commands
-     └── Core
-         └── owner.jsasync execute(interaction, guildConfig, client) {
-    console.log("OWNER COMMAND RAN");
 
-    await interaction.reply({
-        content: "Owner command works!",
-        ephemeral: true
-    });
 }
 import {
     SlashCommandBuilder,

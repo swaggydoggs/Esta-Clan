9227d929497e713bd3634a394acd0a32a73a9566
swaggydoggs <shaloma885@gmail.com>
2026-07-11 19:31:35 -0500

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -103,7 +103,7 @@ export default {
             }
 
             await command.execute(interaction, guildConfig, client);
-          } catch (error) {            await handleInteractionError(interaction, error, withTraceContext({
+       {            await handleInteractionError(interaction, error, withTraceContext({
               type: 'command',
               commandName: interaction.commandName
             }, interactionTraceContext));
